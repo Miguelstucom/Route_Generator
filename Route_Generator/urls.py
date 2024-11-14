306b8813f033f2ac706cc420cf63b460fb6c66fc
Miguel Ángel Vázquez González <miguel.stucom@gmail.com>
2024-11-14 18:53:04 +0100

    commit

---
+++ Route_Generator/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+from Route_Generator import views
+
+
+urlpatterns = [
+    path('index/', views.main_view, name='ejemplo'),
+    # Agrega otros patrones de URL aquí
+]
