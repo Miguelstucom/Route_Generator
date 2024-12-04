863d00c25e40e5abde73645876c1bcfdd940e043
Abel1002 <abelst1002@gmail.com>
2024-12-04 17:29:36 +0100

    changes

--- Route_Generator/views.py
+++ Route_Generator/views.py
@@ -8,11 +8,19 @@ from django.shortcuts import render
 from datetime import date
 from .models import Pedido, Ciudad, Conexion
 from .utils import agrupar_pedidos, calcular_ruta_optima, filtrar_pedidos_validos
-from .utils import calcular_ruta_mas_corta, agrupar_pedidos, verificar_restricciones_tiempo, optimizar_camiones,algoritmo_genetico
+from .utils import (
+    calcular_ruta_mas_corta,
+    agrupar_pedidos,
+    verificar_restricciones_tiempo,
+    optimizar_camiones,
+    algoritmo_genetico,
+)
 from django.template.loader import render_to_string
 from xhtml2pdf import pisa
+
+
 def main_view(request):
-    return render(request, 'Route_Generator/index.html')
+    return render(request, "Route_Generator/index.html")
 
 
 def precargar_distancias(conexiones_file):
@@ -44,12 +52,34 @@ def optimizar_reparto(request):
     distancias = precargar_distancias(conexiones_file)
 
     # Optimizar los camiones con la función de agrupamiento
-    mejor_solucion = optimizar_camiones(pedidos, capacidad_camion)
+    mejor_solucion = optimizar_camiones(pedidos, capacidad_camion, distancias)
+
+    # Inicializar las variables de fechas en None o con valores extremos adecuados
+    fecha_limite_minima = None
+    fecha_disponible_maxima = None
+
+    # Recorrer los pedidos en el camión
+    for pedido in camion:
+        # Obtener las fechas de cada pedido
+        fecha_limite = Pedido.fecha_limite_entrega(pedido)
+        fecha_envio = Pedido.fecha_disponible(pedido)
+
+        # Comparar y encontrar la fecha límite más pequeña
+        if fecha_limite_minima is None or fecha_limite < fecha_limite_minima:
+            fecha_limite_minima = fecha_limite
+
+        # Comparar y encontrar la fecha de envío más grande
+        if fecha_disponible_maxima is None or fecha_envio > fecha_disponible_maxima:
+            fecha_disponible_maxima = fecha_envio
 
     rutas_por_camion = []
     camiones_con_indices = []
     for camion_idx, camion in enumerate(mejor_solucion, start=1):
-        destinos = ["Mataró"] + [pedido.ciudad_destino.nombre for pedido in camion] + ["Mataró"]
+        destinos = (
+            ["Mataró"]
+            + [pedido.ciudad_destino.nombre for pedido in camion]
+            + ["Mataró"]
+        )
 
         # Generar la ruta del camión
         ruta_completa = []
@@ -62,16 +92,17 @@ def optimizar_reparto(request):
         rutas_por_camion.append(ruta_completa)
 
         # Guardar el camión con índice
-        camiones_con_indices.append({
-            "indice": camion_idx,
-            "camion": camion,
-            "ruta": ruta_completa
-        })
+        camiones_con_indices.append(
+            {"indice": camion_idx, "camion": camion, "ruta": ruta_completa}
+        )
 
-    return render(request, "Route_Generator/reparto.html", {
-        "camiones_con_indices": camiones_con_indices,
-    })
-
+    return render(
+        request,
+        "Route_Generator/reparto.html",
+        {
+            "camiones_con_indices": camiones_con_indices,
+        },
+    )
 
 
 def mostrar_mapa(request):
@@ -85,12 +116,12 @@ def mostrar_mapa(request):
 
     coordinates = {}
     for i, row in data.iterrows():
-        coordinates[row['Capital']] = (row['Latitud'], row['Longitud'])
+        coordinates[row["Capital"]] = (row["Latitud"], row["Longitud"])
 
     for i, row in data.iterrows():
         folium.Marker(
             location=[row["Latitud"], row["Longitud"]],
-            popup=f"{row['Capital']} - {row['Provincia']}"
+            popup=f"{row['Capital']} - {row['Provincia']}",
         ).add_to(mapa)
 
     G = nx.Graph()
@@ -110,36 +141,40 @@ def mostrar_mapa(request):
             coord2 = coordinates[capital2]
 
             folium.PolyLine(
-                locations=[coord1, coord2],
-                color="blue",
-                weight=2.5,
-                opacity=1
+                locations=[coord1, coord2], color="blue", weight=2.5, opacity=1
             ).add_to(mapa)
 
     def obtener_ruta_mas_corta(ciudad1, ciudad2):
         try:
-            ruta = nx.shortest_path(G, source=ciudad1, target=ciudad2, weight='weight')
-            distancia = nx.shortest_path_length(G, source=ciudad1, target=ciudad2, weight='weight')
+            ruta = nx.shortest_path(G, source=ciudad1, target=ciudad2, weight="weight")
+            distancia = nx.shortest_path_length(
+                G, source=ciudad1, target=ciudad2, weight="weight"
+            )
             return ruta, distancia
         except nx.NetworkXNoPath:
             return None, None
 
-    if request.method == 'POST':
-        ciudad_origen = 'Mataró'
-        ciudad_destino = request.POST.get('ciudad_destino', None)
+    if request.method == "POST":
+        ciudad_origen = "Mataró"
+        ciudad_destino = request.POST.get("ciudad_destino", None)
 
         if ciudad_origen and ciudad_destino:
             ruta, distancia = obtener_ruta_mas_corta(ciudad_origen, ciudad_destino)
             if ruta:
-                html_content = render_to_string('Route_Generator/pdf.html', {
-                    'ciudad_origen': ciudad_origen,
-                    'ciudad_destino': ciudad_destino,
-                    'ruta': ruta,
-                    'distancia': distancia
-                })
+                html_content = render_to_string(
+                    "Route_Generator/pdf.html",
+                    {
+                        "ciudad_origen": ciudad_origen,
+                        "ciudad_destino": ciudad_destino,
+                        "ruta": ruta,
+                        "distancia": distancia,
+                    },
+                )
 
-                response = HttpResponse(content_type='application/pdf')
-                response['Content-Disposition'] = f'attachment; filename="ruta_{ciudad_origen}_{ciudad_destino}.pdf"'
+                response = HttpResponse(content_type="application/pdf")
+                response["Content-Disposition"] = (
+                    f'attachment; filename="ruta_{ciudad_origen}_{ciudad_destino}.pdf"'
+                )
 
                 pisa_status = pisa.CreatePDF(html_content, dest=response)
 
@@ -148,28 +183,34 @@ def mostrar_mapa(request):
 
                 return response
             else:
-                mensaje = f"No hay ruta disponible entre {ciudad_origen} y {ciudad_destino}."
-                return render(request, 'Route_Generator/index.html', {'mensaje': mensaje})
+                mensaje = (
+                    f"No hay ruta disponible entre {ciudad_origen} y {ciudad_destino}."
+                )
+                return render(
+                    request, "Route_Generator/index.html", {"mensaje": mensaje}
+                )
         else:
             mensaje = "Por favor, ingrese dos ciudades para calcular la ruta."
-            return render(request, 'Route_Generator/index.html', {'mensaje': mensaje})
+            return render(request, "Route_Generator/index.html", {"mensaje": mensaje})
 
     map_path = "Route_Generator/static/mapas/mapa_espana.html"
     mapa.save(map_path)
 
-    return render(request, 'Route_Generator/index.html', {'map_path': map_path})
+    return render(request, "Route_Generator/index.html", {"map_path": map_path})
 
 
 def calcular_distancia(origen, destino, conexiones_file):
     """Devuelve la distancia entre dos ciudades."""
     conexiones_data = pd.read_csv(conexiones_file)
     fila = conexiones_data[
-        (conexiones_data["Capital_1"] == origen) & (conexiones_data["Capital_2"] == destino)
+        (conexiones_data["Capital_1"] == origen)
+        & (conexiones_data["Capital_2"] == destino)
     ]
     if fila.empty:
         # Si no se encuentra conexión directa, prueba en el otro sentido
         fila = conexiones_data[
-            (conexiones_data["Capital_1"] == destino) & (conexiones_data["Capital_2"] == origen)
+            (conexiones_data["Capital_1"] == destino)
+            & (conexiones_data["Capital_2"] == origen)
         ]
     if not fila.empty:
         return float(fila.iloc[0]["Peso"])
